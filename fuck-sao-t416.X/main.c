6422c7e761c3ffcf97799bc1727360059a41f08f
BrandenBoucher <bgbouche@asu.edu>
2024-02-12 22:12:42 -0800

    added comments

--- fuck-sao-t416.X/main.c
+++ fuck-sao-t416.X/main.c
@@ -10,16 +10,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+//All the pins that are outputs for LEDs
 #define PORTA_PINS (PIN1_bm | PIN2_bm | PIN3_bm | PIN4_bm | PIN5_bm | PIN6_bm | PIN7_bm)
 #define PORTB_PINS (PIN0_bm | PIN1_bm | PIN2_bm | PIN3_bm | PIN4_bm | PIN5_bm)
+//The pin for the button
 #define BUTTON_PIN PIN0_bm
+//BS debug pins that will never be used
 #define DEBUG_PIN_IN PIN1_bm
 #define DEBUG_PIN_OUT (PIN2_bm | PIN3_bm)
 
 uint8_t index = 0;
 uint16_t counter = 0;
-uint8_t button_state = 0;
+uint8_t button_activated = 0;
 
+//Update to the 20mHz clock
 void init_clk()
 {
 	CCP = CCP_IOREG_gc;
@@ -28,16 +32,20 @@ void init_clk()
 	CLKCTRL.MCLKCTRLB = 0;
 }
 
+//Set up the pins for input and output
 void init_io() {
     PORTA.DIRSET = PORTA_PINS;
     PORTB.DIRSET = PORTB_PINS;
     PORTC.DIRCLR = BUTTON_PIN | DEBUG_PIN_IN;
+    //The button grounds the pin so it needs to be pulled high
     PORTC.PIN0CTRL = PORT_PULLUPEN_bm;
     PORTC.DIRSET = DEBUG_PIN_OUT;
 }
 
+//Lazy switch statement to turn on the selected LED
 void switch_led(uint8_t index) {
     
+    //Turn them all off first
     PORTA.OUTCLR = PORTA_PINS;
     PORTB.OUTCLR = PORTB_PINS;    
     
@@ -87,20 +95,25 @@ void switch_led(uint8_t index) {
     }
 }
 
-uint8_t button_activated = 0;
-
 void main(void) {
     
+    //Init shit
     init_clk();
-    
     init_io();
+    
+    //Turn on the first led
     index = 0;
     switch_led(index);
     
+    
     while(1) {
+        //Gotta slow down this loop in the laziest way possible
         _delay_ms(1);
+        
+        //This means the button is not pressed
         if (PORTC.IN & PIN0_bm) {
             
+            //Pin was debounced so update the LED
             if (button_activated) {
                 index++;
                 switch_led(index);
@@ -119,7 +132,7 @@ void main(void) {
                 switch_led(index);
                 if (index > 12) index = 0;
             }
-            else if (counter > 50 && !button_activated) {
+            else if (counter > 50 && !button_activated) { //Regular hold
                 button_activated = 1;
             } 
 
